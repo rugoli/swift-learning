43cf70c0b3af8f21a9b88206868d8f67026c741b
Roshan <none@none>
2017-11-02 16:50:33 -0700

    getting buttons to show up properly

--- DogFoodCalculator/DogFoodCalculator/Views/DFCalculatorCollectionViewCell.swift
+++ DogFoodCalculator/DogFoodCalculator/Views/DFCalculatorCollectionViewCell.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class DFCalculatorCollectionViewCell: UICollectionViewCell {
     private let cellMainLabel: UILabel
-    private let cellSecondaryLabel: UILabel
+    private let supportedUnitsRow: DFSupportedMeasurementUnitsRow
     private var ingredientModel: DFIngredientModel!
     
     static let reuseIdentifier: String = "calculator-cell"
     
     override init(frame: CGRect) {
         cellMainLabel = UILabel()
-        cellSecondaryLabel = UILabel()
+        supportedUnitsRow = DFSupportedMeasurementUnitsRow()
         
         super.init(frame: frame)
         self.backgroundColor = UIColor.white
         self.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         
         self.addSubview(cellMainLabel)
-        self.addSubview(cellSecondaryLabel)
+        self.addSubview(supportedUnitsRow)
     }
 
     required convenience init?(coder aDecoder: NSCoder) {
@@ -35,24 +35,7 @@ class DFCalculatorCollectionViewCell: UICollectionViewCell {
         super.layoutSubviews()
         
         self.setMainLabelConstraints()
-        self.setSecondaryLabelConstraints()
-    }
-    
-    func configureCellWithModel(_ ingredient: DFIngredientModel) {
-        ingredientModel = ingredient
-        self.configureMainLabelProperties()
-        self.configureSecondaryLabelProperties()
-        self.setNeedsLayout()
-    }
-    
-    // MARK:
-    
-    private func configureMainLabelProperties() {
-        cellMainLabel.text = ingredientModel.ingredientName
-        cellMainLabel.textColor = UIColor.blue
-        cellMainLabel.numberOfLines = 0
-        cellMainLabel.textAlignment = NSTextAlignment.center
-        cellMainLabel.font = cellMainLabel.font.withSize(18)
+        self.setSupportedUnitsRowConstraints()
     }
     
     private func setMainLabelConstraints() {
@@ -68,26 +51,41 @@ class DFCalculatorCollectionViewCell: UICollectionViewCell {
         NSLayoutConstraint.activate([centering, topPadding, width, height])
     }
     
-    // MARK:
-    
-    private func configureSecondaryLabelProperties() {
-        cellSecondaryLabel.text = "Just a test"
-        cellSecondaryLabel.textColor = UIColor.red
-        cellSecondaryLabel.numberOfLines = 1
-        cellSecondaryLabel.textAlignment = NSTextAlignment.center
-        cellSecondaryLabel.font = cellMainLabel.font.withSize(14)
-    }
-    
-    private func setSecondaryLabelConstraints() {
+    private func setSupportedUnitsRowConstraints() {
         let cellSize = self.bounds.size
         let cellMargins = self.layoutMargins
-        let labelSize: CGSize = cellSecondaryLabel.sizeThatFits(CGSize(width: cellSize.width - cellMargins.left - cellMargins.right, height: cellSize.height))
+        let rowSize: CGSize = CGSize(width: cellSize.width - cellMargins.left - cellMargins.right, height: 40)
         
-        cellSecondaryLabel.translatesAutoresizingMaskIntoConstraints = false
-        let centering = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: cellSecondaryLabel, attribute: NSLayoutAttribute.centerX, multiplier: 1.0, constant: 0)
-        let topSpacing = NSLayoutConstraint(item: cellSecondaryLabel, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: cellMainLabel, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 5)
-        let width = NSLayoutConstraint(item: cellSecondaryLabel, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: labelSize.width)
-        let height = NSLayoutConstraint(item: cellSecondaryLabel, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: labelSize.height)
-        NSLayoutConstraint.activate([centering, topSpacing, width, height])
+        supportedUnitsRow.translatesAutoresizingMaskIntoConstraints = false
+        let centering = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: supportedUnitsRow, attribute: NSLayoutAttribute.centerX, multiplier: 1.0, constant: 0)
+        let topSpacing = NSLayoutConstraint(item: supportedUnitsRow, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: cellMainLabel, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 5)
+        let height = NSLayoutConstraint(item: supportedUnitsRow, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: rowSize.height)
+        NSLayoutConstraint.activate([centering, topSpacing, height])
+    }
+}
+
+// MARK:
+
+// Configuring views and models
+
+extension DFCalculatorCollectionViewCell {
+    
+    func configureCellWithModel(_ ingredient: DFIngredientModel) {
+        ingredientModel = ingredient
+        self.configureMainLabelProperties()
+        self.configureSupportedUnitsRow()
+        self.setNeedsLayout()
+    }
+    
+    private func configureMainLabelProperties() {
+        cellMainLabel.text = ingredientModel.ingredientName
+        cellMainLabel.textColor = UIColor.blue
+        cellMainLabel.numberOfLines = 0
+        cellMainLabel.textAlignment = NSTextAlignment.center
+        cellMainLabel.font = cellMainLabel.font.withSize(18)
+    }
+    
+    private func configureSupportedUnitsRow() {
+        supportedUnitsRow.configureSupportedMeasurementUnits(ingredientModel.supportedMeasurementUnits)
     }
 }
