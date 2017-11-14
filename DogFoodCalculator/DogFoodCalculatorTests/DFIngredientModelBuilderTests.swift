65eaa02209a776e2e734ec3b988165b49c220b88
Roshan <none@none>
2017-11-13 21:42:06 -0800

    model builder tests

---
+++ DogFoodCalculator/DogFoodCalculatorTests/DFIngredientModelBuilderTests.swift
@@ -0,0 +1,32 @@
+//
+//  DFIngredientModelBuilderTests.swift
+//  DogFoodCalculatorTests
+//
+//  Created by Roshan Goli on 11/13/17.
+//  Copyright © 2017 Roshan Goli. All rights reserved.
+//
+
+import XCTest
+@testable import DogFoodCalculator
+
+class DFIngredientModelBuilderTests: XCTestCase {
+    
+  func testModelChange() {
+    let testModel = DFIngredientModel(ingredientName: "Test", supportedMeasurementUnits: [DFMeasurementUnit.cup], defaultMeasurementUnit: DFMeasurementUnit.oz)
+    
+    let newIngredientName = "Test 3"
+    let newModel = DFIngredientModelBuilder(fromModel: testModel)
+      .withIngredientName(newIngredientName)
+      .build()
+    XCTAssertFalse(testModel == newModel) // models are no longer the same
+    XCTAssertTrue(newModel.ingredientName == newIngredientName) // name has changed
+    XCTAssertTrue(testModel.defaultMeasurementUnit == newModel.defaultMeasurementUnit) // another property did not change by accident
+  }
+  
+  func testNoModelChange() {
+    let testModel = DFIngredientModel(ingredientName: "Test", supportedMeasurementUnits: [DFMeasurementUnit.cup], defaultMeasurementUnit: DFMeasurementUnit.oz)
+    let newModel = DFIngredientModelBuilder(fromModel: testModel).build()
+    XCTAssertTrue(testModel.isEffectivelyEqualTo(newModel))
+  }
+    
+}
